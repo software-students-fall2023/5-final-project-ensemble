6608efb7b5842550b2559427ca61bf11187f9900
geoffreybudiman91 <geoffrey.budiman@gmail.com>
2023-12-07 22:37:50 -0500

    Finished Home Page

--- web-app/server/app.py
+++ web-app/server/app.py
@@ -10,8 +10,9 @@ import re
 
 load_dotenv()
 user = os.environ["MONGO_INITDB_ROOT_USERNAME"]
-passw = os.environ["MONGO_INITDB_ROOT_PASSWORD"]
-uri = f"mongodb://{user}:{passw}@mongo:27017/db?authSource=admin"
+password = os.environ["MONGO_INITDB_ROOT_PASSWORD"]
+# uri = f"mongodb://{user}:{password}@localhost:27017/inventory_db"
+uri = f"mongodb://ensemble:l2Ro7dr7DzpQ@localhost:27017"
 
 mongo = MongoClient(uri, server_api=ServerApi('1'))
 
@@ -21,16 +22,29 @@ try:
 except pymongo.errors.ConnectionFailure as e:
     print(e)
 
-app=Flask(__name__, template_folder='../templates', static_folder='../static')
+app=Flask(__name__, template_folder='../client/templates', static_folder='../client/static')
 
 @app.route('/')
 def home():
-    inventory = list(mongo.db.inventory.find())
-    inventory.sort(key=lambda x: int(x['SKU']))
+    """Render home page."""
+    inventory = list(mongo.inventory_db.inventory.find())
+    inventory.sort(key=lambda x: int(x['sku']))
     return render_template('home.html', inventory=inventory)
 
 @app.route('/search')
 def search():
+    """Implement search bar functionality"""
     search_query = request.args.get('query')
-    inventory = mongo.db.inventory.find({"SKU": re.compile(search_query, re.IGNORECASE)})
+    try:
+        sku_query = int(search_query)
+        sku_match = {"sku": sku_query}
+    except ValueError:
+        sku_match = {}
+
+    name_match = {"product_name": re.compile(search_query, re.IGNORECASE)}
+    query = {"$or": [sku_match, name_match]} if sku_match else name_match
+    inventory = mongo.inventory_db.inventory.find(query)
+
     return render_template('home.html', inventory=inventory)
+
+app.run(debug=True)
